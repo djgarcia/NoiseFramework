3bd4407f33dbef113ea283cb967ee637ed749b1b
Diego García <djgarcia@decsai.ugr.es>
2017-07-06 12:02:11 +0200

    Update HTE_BD.scala

--- src/main/scala/org/apache/spark/mllib/feature/HTE_BD.scala
+++ src/main/scala/org/apache/spark/mllib/feature/HTE_BD.scala
@@ -14,7 +14,7 @@ class HTE_BD(val data: RDD[LabeledPoint], val nTrees: Int, val k: Int, val vote:
 
   def runFilter(): RDD[LabeledPoint] = {
 
-    val cvdat = MLUtils.kFold(data, k, 12345)
+    val cvdat = MLUtils.kFold(data, k, seed)
 
     //RF Parameters
     val numClasses = labels.length
